521e2ec41c7d30604c273c30cf023617566224a3
kmi8a <168780154+kmi8a@users.noreply.github.com>
2026-06-16 15:50:50 -0400

    Title added

--- dashboard.py
+++ dashboard.py
@@ -14,7 +14,7 @@ df = pd.read_sql("SELECT * FROM bogota;", conn)
 df['Date'] = pd.to_datetime(df['Date'])
 
 # Title
-st.title('Bogota Weather Dashboard')
+st.title('Bogota, Colombia - Weather Dashboard')
 st.text("Comparing data from Sept 2009 to May 2026") 
 
 # Color mapping
