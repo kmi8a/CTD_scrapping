eb6635ef165a186d81597df7cb2c3c381346ef98
kmi8a <168780154+kmi8a@users.noreply.github.com>
2026-06-16 15:24:01 -0400

    temp symbols on labels

--- dashboard.py
+++ dashboard.py
@@ -30,11 +30,13 @@ filtered_df = df[df['Insight'].isin(selected_types)]
 # Visualization 1 - Temperature Trends
 st.subheader('Temperature Trends')
 fig_line = px.line(filtered_df, x='Date', y='Value', color='Insight', color_discrete_map=color_map, markers=True)
+fig_line.update_layout(yaxis_title='Temperature (°F)')
 st.plotly_chart(fig_line, use_container_width=True)
 
 # Visualization 2 - Histogram
 st.subheader("Distribution of Temperatures (Frequency)")
 fig_hist = px.histogram(filtered_df, x='Value', color='Insight', nbins=20, color_discrete_map=color_map)
+fig_line.update_layout(yaxis_title='Temperature (°F)')
 st.plotly_chart(fig_hist, use_container_width=True)
 
 #Visualization 3 - Heatmap
